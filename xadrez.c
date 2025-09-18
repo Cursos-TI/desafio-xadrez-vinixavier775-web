93bd117c182921135c7e1180d4f66bb1bf409a57
Vinixavier81 <vinixavier775@gmail.com>
2025-09-18 22:17:40 +0000

    Novo Commit!

--- xadrez.c
+++ xadrez.c
@@ -1,39 +1,59 @@
 #include <stdio.h>
 
+//Recursividade
+//Torre   
+void MoverTorre(int casas) {
+    if (casas > 0)
+    {
+        printf("Torre move-se para a Direita!\n");
+        MoverTorre(casas - 1);
+    }
+}
+
+//Bispo 
+void MoverBispo(int casas) {
+    if (casas > 0)
+    {
+        printf("Bispo move-se para cima a esquerda!\n");
+        MoverBispo(casas - 1);
+    }
+    
+}
+
+//Rainha
+void MoverRainha(int casas) {
+    if (casas > 0)
+    {
+        printf("Rainha move-se para a esquerda!\n");
+        MoverRainha(casas - 1);
+    }
+        
+}
 int main(){
     //Variavel
     int i = 1;
     int cavalo = 1;
 
-    //Movimento da Torre
-    while (i <= 5)
-    {
-        printf("Torre move-se para a direita!\n");
-        i++;
+   MoverTorre(5);
+
+   MoverBispo(5);
+   
+   MoverRainha(8);
+    
+   //Loop Avançado para o cavalo
+   for (int i = 1; i < 4; i++) {
+        if (i == 3) continue;
+        if (i == 4) break;
+        printf("Cavalo se move para cima\n");
     }
-
-    //Movimento do Bispo
-    do
+    for (i = 1; i < 3; i++)
     {
-        printf("Bispo move-se para cima a esquerda!\n");
-        i++;
-    } while (i <= 10);
-
-    //Movimento da Rainha
-    for (i = 1; i <= 8; i++)
-    {
-        printf("Rainha move-se para a esquerda!\n");
+        if (i == 2) continue;
+        if (i == 3) break;
+        
+        printf("Cavalo se move para direita\n");
     }
     
-    //Movimento do Cavalo
-    while (cavalo--)
-    {
-        for (i = 0; i < 2; i++)
-        {
-            printf("Cavalo se move para baixo!\n");
-        }
-        printf("Cavalo se move para a esquerda!\n");
-    }
     
     
     return 0;
